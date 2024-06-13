ebe4b46b8840ff6cbc1fcefc49c05fec52839297
Ivan <94743980+xzripper@users.noreply.github.com>
2024-06-13 22:25:57 +0300

    Vendor.
    Vendor.

---
+++ vsdk/sdk_vendor/jlocalizer/Localization.java
@@ -0,0 +1,58 @@
+// JLocalizer by Violent Studio.
+
+package vsdk.sdk_vendor.jlocalizer;
+
+/**
+ * Localization class.
+ */
+public class Localization {
+    protected LocalizationPack locPack;
+
+    protected String defaultLanguage = null;
+
+    /**
+     * Initialize localization manager.
+     */
+    public Localization(LocalizationPack locPack_) {
+        locPack = locPack_;
+    }
+
+    /**
+     * Is pack loaded successfully.
+     */
+    public boolean isLoaded() {
+        return locPack.isLoaded();
+    }
+
+    /**
+     * Set language by default.
+     */
+    public void setDefaultLanguage(String defaultLanguage_) {
+        defaultLanguage = defaultLanguage_;
+    }
+
+    /**
+     * Get default language.
+     */
+    public String getDefaultLanguage() {
+        return defaultLanguage;
+    }
+
+    /**
+     * Get localized string.
+     */
+    public String getString(String name, String language) {
+        return locPack.getMap().get(String.format("%s_%s", name, language));
+    }
+
+    /**
+     * Get localized string.
+     */
+    public String getString(String name) {
+        if(defaultLanguage != null) {
+            return getString(name, defaultLanguage);
+        }
+
+        return null;
+    }
+}
