a186e34b3ad209e88b34fa7ac062752bf2e0b9d4
Ivan <94743980+xzripper@users.noreply.github.com>
2024-06-14 23:51:39 +0300

    Update.
    Update.

--- vsdk/source/WindowIconUtil.java
+++ vsdk/source/WindowIconUtil.java
@@ -2,6 +2,8 @@ package vsdk.source;
 
 import com.raylib.Raylib;
 
+import static vsdk.r_utilities.PathResolver.resolvePath;
+
 /**
  * Simple one-method utility for setting icon for window.
  */
@@ -10,8 +12,11 @@ public class WindowIconUtil {
      * Set window icon.
      *
      * @param icon Path to icon.
+     * @param resolve Resolve path?
      */
-    public static void setWinIcon(String icon) {
+    public static void setWinIcon(String icon, boolean resolve) {
+        if(resolve) icon = resolvePath(icon);
+
         Raylib.Image iconImg = Raylib.LoadImage(icon);
 
         Raylib.SetWindowIcon(iconImg);
