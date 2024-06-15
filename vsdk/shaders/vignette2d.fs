8b869aa186e298b2bc3909f23e3ac61e6cc03027
Ivan <94743980+xzripper@users.noreply.github.com>
2024-06-15 19:13:20 +0300

    2D Vignette Shader.
    2D Vignette Shader.

---
+++ vsdk/shaders/vignette2d.fs
@@ -0,0 +1,17 @@
+#version 330
+
+in vec2 fragTexCoord;
+out vec4 finalColor;
+
+uniform sampler2D texture0;
+
+uniform float vRadius;
+uniform float vBlur;
+uniform vec3 vColor;
+
+uniform float vPosX;
+uniform float vPosY;
+
+void main() {
+    finalColor = mix(texture(texture0, fragTexCoord), vec4(vColor, 1.0), smoothstep(vRadius, vRadius + vBlur, distance(fragTexCoord, vec2(vPosX, vPosY))));
+}
