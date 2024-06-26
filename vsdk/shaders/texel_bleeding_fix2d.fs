ae552593e718ad0fd51470f60db47a2c184ad239
Ivan <94743980+xzripper@users.noreply.github.com>
2024-06-26 15:37:57 +0300

    Texture Bleeding Fix 2D Shader.
    Texture Bleeding Fix 2D Shader.

---
+++ vsdk/shaders/texel_bleeding_fix2d.fs
@@ -0,0 +1,16 @@
+// VSDK Shader to fix texture bleeding in Raylib: https://github.com/raysan5/raylib/issues/3247
+
+#version 330
+
+in vec2 fragTexCoord;
+in vec4 fragColor;
+
+out vec4 finalColor;
+
+uniform sampler2D texture0;
+
+uniform vec4 colDiffuse;
+
+void main() {
+    finalColor = texture(texture0, fragTexCoord).rrrg * colDiffuse * fragColor;
+}
