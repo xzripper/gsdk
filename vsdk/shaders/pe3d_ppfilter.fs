ccb288643e707710be7603a09dd44caf6cf0290a
Ivan <94743980+xzripper@users.noreply.github.com>
2024-07-14 01:14:22 +0300

    `discard_alpha2d.fs` -> `pe3d_ppfilters.fs`
    discard_alpha2d.fs -> pe3d_ppfilters.fs

---
+++ vsdk/shaders/pe3d_ppfilter.fs
@@ -0,0 +1,24 @@
+// VSDK Fragment Shader for Particle Emitter 3D for pixels filtering.
+
+#version 330
+
+in vec2 fragTexCoord;
+in vec4 fragColor;
+
+uniform sampler2D texture0;
+
+uniform vec4 colDiffuse;
+
+uniform vec4 threshold;
+
+out vec4 finalColor;
+
+void main() {
+    vec4 texelColor = texture(texture0, fragTexCoord);
+
+    if((texelColor.r < threshold.x && texelColor.g < threshold.y && texelColor.b < threshold.z) || texelColor.a < threshold.w) {
+        discard;
+    }
+
+    finalColor = texelColor * colDiffuse * fragColor;
+}
