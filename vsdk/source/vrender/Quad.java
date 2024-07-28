f5889813e723e181b91d87b42663793ed0149455
Ivan <94743980+xzripper@users.noreply.github.com>
2024-07-29 02:05:22 +0300

    Quad renderer.

---
+++ vsdk/source/vrender/Quad.java
@@ -0,0 +1,126 @@
+package vsdk.source.vrender;
+
+import vsdk.source.vectors.Vector3Df;
+import vsdk.source.vectors.Vector2Df;
+
+import static vsdk.source.vectors.Vector3Df.vec3df;
+
+import com.raylib.Raylib;
+
+/**
+ * Class for rendering quads.
+ */
+public class Quad {
+    /**
+     * Generate quad vertex coordinates.
+     * 
+     * @param size Quad size.
+     * @param pos Quad position.
+     * @param rotation Quad rotation.
+     */
+    public static Vector3Df[] genQuadVertCoords(Vector2Df size, Vector3Df pos, Vector3Df rotation) {
+        Vector3Df[] vertices = new Vector3Df[] {
+            vec3df(-size.x() / 2.0f, 0, -size.y() / 2.0f),
+            vec3df(size.x() / 2.0f, 0, -size.y() / 2.0f),
+            vec3df(size.x() / 2.0f, 0, size.y() / 2.0f),
+            vec3df(-size.x() / 2.0f, 0, size.y() / 2.0f)
+        };
+
+        double rotX = Math.toRadians(rotation.x()), rotY = Math.toRadians(rotation.y()), rotZ = Math.toRadians(rotation.z());
+    
+        for(int i = 0; i < vertices.length; i++) {
+            Vector3Df vert = vertices[i];
+    
+            float tY = vert.y();
+
+            vert.y((float) (vert.y() * Math.cos(rotX) - vert.z() * Math.sin(rotX)));
+            vert.z((float) (tY * Math.sin(rotX) + vert.z() * Math.cos(rotX)));
+    
+            float tX = vert.x();
+
+            vert.x((float) (vert.x() * Math.cos(rotY) + vert.z() * Math.sin(rotY)));
+            vert.z((float) (-tX * Math.sin(rotY) + vert.z() * Math.cos(rotY)));
+    
+            tX = vert.x();
+
+            vert.x((float) (vert.x() * Math.cos(rotZ) - vert.y() * Math.sin(rotZ)));
+            vert.y((float) (tX * Math.sin(rotZ) + vert.y() * Math.cos(rotZ)));
+    
+            vertices[i] = vec3df(vert.x() + pos.x(), vert.y() + pos.y(), vert.z() + pos.z());
+        }
+    
+        return vertices;
+    }
+
+    /**
+     * Generate quad vertex coordinates with zero origin and rotation.
+     * 
+     * @param size Quad size.
+     * @param pos Quad position.
+     */
+    public static Vector3Df[] genQuadVertCoords(Vector2Df size, Vector3Df pos) {
+        return genQuadVertCoords(size, pos, vec3df(0, 0, 0));
+    }
+
+    /**
+     * Draw 3D Quad mesh.
+     * 
+     * @param size Quad size.
+     * @param pos Quad position.
+     * @param rotation Quad rotation.
+     * @param color Quad color.
+     * @param backfaceCulling Enable/disable backface culling.
+     */
+    public static void drawQuad(Vector2Df size, Vector3Df pos, Vector3Df rotation, Raylib.Color color, boolean backfaceCulling) {
+        Vector3Df[] vertCoords = genQuadVertCoords(size, pos, rotation);
+
+        if(!backfaceCulling) Raylib.rlDisableBackfaceCulling();
+
+        Raylib.DrawTriangle3D(vertCoords[0].toRlVec(), vertCoords[1].toRlVec(), vertCoords[2].toRlVec(), color);
+        Raylib.DrawTriangle3D(vertCoords[0].toRlVec(), vertCoords[2].toRlVec(), vertCoords[3].toRlVec(), color);
+
+        if(!backfaceCulling) Raylib.rlEnableBackfaceCulling();
+    }
+
+    /**
+     * Draw 3D Quad mesh with backface culling.
+     * 
+     * @param size Quad size.
+     * @param pos Quad position.
+     * @param rotation Quad rotation.
+     * @param color Quad color.
+     */
+    public static void drawQuad(Vector2Df size, Vector3Df pos, Vector3Df rotation, Raylib.Color color) {
+        drawQuad(size, pos, color, true);
+    }
+
+    /**
+     * Draw 3D Quad mesh with default rotation.
+     * 
+     * @param size Quad size.
+     * @param pos Quad position.
+     * @param color Quad color.
+     * @param backfaceCulling Enable/disable backface culling.
+     */
+    public static void drawQuad(Vector2Df size, Vector3Df pos, Raylib.Color color, boolean backfaceCulling) {
+        Vector3Df[] vertCoords = genQuadVertCoords(size, pos);
+
+        if(!backfaceCulling) Raylib.rlDisableBackfaceCulling();
+
+        Raylib.DrawTriangle3D(vertCoords[0].toRlVec(), vertCoords[1].toRlVec(), vertCoords[2].toRlVec(), color);
+        Raylib.DrawTriangle3D(vertCoords[0].toRlVec(), vertCoords[2].toRlVec(), vertCoords[3].toRlVec(), color);
+
+        if(!backfaceCulling) Raylib.rlEnableBackfaceCulling();
+    }
+
+    /**
+     * Draw 3D Quad mesh with default rotation and backface culling.
+     * 
+     * @param size Quad size.
+     * @param pos Quad position.
+     * @param color Quad color.
+     */
+    public static void drawQuad(Vector2Df size, Vector3Df pos, Raylib.Color color) {
+        drawQuad(size, pos, color, true);
+    }
+}
