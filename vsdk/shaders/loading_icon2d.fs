fa643076ffbf4429812173c9fef2b05ff36d8dd7
Ivan <94743980+xzripper@users.noreply.github.com>
2024-08-04 01:48:50 +0300

    New loading animations and integration with preprocessing definitions.

--- vsdk/shaders/loading_icon2d.fs
+++ vsdk/shaders/loading_icon2d.fs
@@ -1,4 +1,5 @@
 // VSDK Loading Icon (IVUI) Fragment Shader.
+// Since v0.0.0.6-alpha/dev shader is supported only by VShader (or custom pre-processor definitions system implementation).
 
 #version 330
 
@@ -12,10 +13,27 @@ in vec4 fragColor;
 
 out vec4 finalColor;
 
+#define PI 3.14159265358979323846
+
 void main() {
     vec4 texColor = texture(texture0, fragTexCoord);
 
+    #ifdef top
     texColor.rgb *= fragTexCoord.y > progress ? tint : 1.0;
+    #endif
+
+    #ifdef vertical
+    texColor.rgb *= fragTexCoord.x > progress ? tint : 1.0;
+    #endif
+
+    #ifdef pie
+    float angle = atan(fragTexCoord.y - 0.5, fragTexCoord.x - 0.5) + PI;
+    float radius = length(fragTexCoord - vec2(0.5));
+
+    float progressAngle = 2.0 * PI * progress;
+
+    texColor.rgb *= (radius <= 0.5 && angle <= progressAngle) ? 1.0 : tint;
+    #endif
 
     finalColor = texColor * fragColor;
 }
