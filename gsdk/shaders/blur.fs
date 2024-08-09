bef1256935b9af325d003f6876be2da4e298e450
Ivan <94743980+xzripper@users.noreply.github.com>
2024-08-09 20:06:56 +0200

    blur upd

--- gsdk/shaders/blur.fs
+++ gsdk/shaders/blur.fs
@@ -2,8 +2,7 @@
 #version 330
 in vec2 fragTexCoord;out vec4 finalColor;
 uniform sampler2D texture0;uniform float xs,ys,r;
-void main(){
-float x,y,xx,yy,rr=r*r,dx,dy,w,w0=0.3780/pow(r,1.975);
+void main(){float x,y,xx,yy,rr=r*r,dx,dy,w,w0=0.3780/pow(r,1.975);
 vec2 p;vec4 col=vec4(0.0);for(dx=1.0/xs,x=-r,p.x=fragTexCoord.x+(x*dx);x<=r;x++,p.x+=dx){
 xx=x*x;for(dy=1.0/ys,y=-r,p.y=fragTexCoord.y+(y*dy);y<=r;y++,p.y+=dy){yy=y*y;
 if(xx+yy<=rr){w=w0*exp((-xx-yy)/(2.0*rr));col+=texture(texture0,p)*w;}}}finalColor=col;}
--- gsdk/shaders/blur2.fs
+++ gsdk/shaders/blur2.fs
@@ -2,8 +2,7 @@
 #version 330
 in vec2 fragTexCoord;out vec4 finalColor;
 uniform sampler2D texture0;uniform float xs,ys,r;
-void main(){
-vec2 texOffset=vec2(1.0)/vec2(xs,ys);vec4 color=vec4(0.0);float totalWeight=0.0;
+void main(){vec2 texOffset=vec2(1.0)/vec2(xs,ys);vec4 color=vec4(0.0);float totalWeight=0.0;
 for(float x=-r;x<=r*32;++x){for(float y=-r;y<=r*32;++y){float weight=exp(-(float(x*x)+float(y*y))/(2.0*r*r));
 color+=texture(texture0,fragTexCoord+vec2(float(x)*texOffset.x,float(y)*texOffset.y))*weight;
 totalWeight+=weight;}}finalColor=color/totalWeight;}
